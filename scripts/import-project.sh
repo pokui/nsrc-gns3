20ac3f62de4b1b123907a5b1629dfe5752891668
Brian Candler <brian@nsrc.org>
2018-11-09 19:49:31 +0000

    Sort JSON so that it diffs cleanly

--- scripts/import-project.sh
+++ scripts/import-project.sh
@@ -11,5 +11,5 @@ PROJFILE="$PROJECT.gns3project"
 
 zip -d "$PROJFILE" 'project-files/*' || true
 unzip -d "$PROJECT" -o "$PROJFILE"
-python3 -m json.tool "$PROJECT/project.gns3" "$PROJECT/project.gns3.new"
+python3 -m json.tool --sort-keys "$PROJECT/project.gns3" "$PROJECT/project.gns3.new"
 mv "$PROJECT/project.gns3.new" "$PROJECT/project.gns3"
