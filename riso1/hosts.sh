ddd4956af9028c79b97d7e545ed817f49b6fbfba
Brian Candler <brian@nsrc.org>
2020-05-07 08:36:28 +0100

    Update riso hosts.sh to fix P2P links and add TX

--- riso1/hosts.sh
+++ riso1/hosts.sh
@@ -28,7 +28,7 @@ for TR in $(seq 1 2); do
 2001:DB8::$(( TR+1 ))	gi0-0.tr${TR}.ws.nsrc.org
 2001:18::$(( TR-1 ))	gi0-0.tr${TR}.ws.nsrc.org
 EOS
-  for G in $(seq 1 9); do
+  for G in $(seq 1 5); do
     cat <<EOS
 100.$(( TR+120 )).1.$(( (G-1)*2 ))	gi0-$G.tr${TR}.ws.nsrc.org
 2001:$(( TR+17 )):0:$(( G+9 ))::	gi0-$G.tr${TR}.ws.nsrc.org
@@ -49,8 +49,8 @@ cat <<EOS
 2001:db8:fffe:1::fe	ens4.rs2.ws.nsrc.org
 192.168.122.6		ens5.rs2.ws.nsrc.org rs2.ws.nsrc.org rs2
 
-100.121.1.17	gi0-0.sr1.ws.nsrc.org
-2001:18:0:18::1	gi0-0.sr1.ws.nsrc.org
+100.121.1.9	gi0-0.sr1.ws.nsrc.org
+2001:18:0:14::1	gi0-0.sr1.ws.nsrc.org
 100.127.0.9	gi0-1.sr1.ws.nsrc.org
 2001:DB8:FFFF:2::9	gi0-1.sr1.ws.nsrc.org
 100.127.1.253	gi0-2.sr1.ws.nsrc.org
@@ -58,8 +58,8 @@ cat <<EOS
 100.127.0.1	gi0-3.sr1.ws.nsrc.org sr1.ws.nsrc.org sr1
 2001:DB8:FFFF::1	gi0-3.sr1.ws.nsrc.org sr1.ws.nsrc.org sr1
 
-100.122.1.17	gi0-0.sr2.ws.nsrc.org
-2001:19:0:18::1	gi0-0.sr2.ws.nsrc.org
+100.122.1.9	gi0-0.sr2.ws.nsrc.org
+2001:19:0:14::1	gi0-0.sr2.ws.nsrc.org
 100.127.2.9	gi0-1.sr2.ws.nsrc.org
 2001:DB8:FFFE:2::9	gi0-1.sr2.ws.nsrc.org
 100.127.3.253	gi0-2.sr2.ws.nsrc.org
@@ -74,14 +74,15 @@ cat <<EOS
 2001:DB8:FFFE::2	ixp2.ws.nsrc.org ixp2
 EOS
 
-for GROUP in $(seq 1 8); do
-  OTHER="$(( ((GROUP-1)^1) + 1 ))"
+for GHI in $(seq 0 1); do
+for GLO in $(seq 0 3); do
+  GROUP=$(( GHI*4 + GLO + 1 ))
 
   # Border calc
-  TRANSIT1_AS="$(( (GROUP-1) / 4 + 121 ))"
-  TRANSIT1_V6="$(( (GROUP-1) / 4 + 18 ))"
-  TRANSIT1_LOCAL_V4="100.${TRANSIT1_AS}.1.$((GROUP*2-1))"
-  TRANSIT1_LOCAL_V6="2001:${TRANSIT1_V6}:0:$((GROUP+9))::1"
+  TRANSIT_AS="$(( GHI + 121 ))"
+  TRANSIT_V6="$(( GHI + 18 ))"
+  TRANSIT_LOCAL_V4="100.${TRANSIT_AS}.1.$((GLO*2+1))"
+  TRANSIT_LOCAL_V6="2001:${TRANSIT_V6}:0:$((GLO+10))::1"
 
   # Private peering calc
   LINK_GROUP="$(( ((GROUP - 1) & -2) + 1 ))"
@@ -105,41 +106,41 @@ for GROUP in $(seq 1 8); do
 
 100.68.${GROUP}.1	lo0.b${GROUP}.ws.nsrc.org b${GROUP}.ws.nsrc.org b${GROUP}
 2001:DB8:${GROUP}::1	lo0.b${GROUP}.ws.nsrc.org b${GROUP}.ws.nsrc.org b${GROUP}
-100.68.${GROUP}.17	gi0-1.b${GROUP}.ws.nsrc.org
-2001:DB8:${GROUP}:10::1	gi0-1.b${GROUP}.ws.nsrc.org
-${TRANSIT1_LOCAL_V4}	gi0-2.b${GROUP}.ws.nsrc.org
-${TRANSIT1_LOCAL_V6}	gi0-2.b${GROUP}.ws.nsrc.org
+100.68.${GROUP}.17	gi1.b${GROUP}.ws.nsrc.org
+2001:DB8:${GROUP}:10::1	gi1.b${GROUP}.ws.nsrc.org
+${TRANSIT_LOCAL_V4}	gi2.b${GROUP}.ws.nsrc.org
+${TRANSIT_LOCAL_V6}	gi2.b${GROUP}.ws.nsrc.org
 
 100.68.${GROUP}.2	lo0.c${GROUP}.ws.nsrc.org c${GROUP}.ws.nsrc.org c${GROUP}
 2001:DB8:${GROUP}::2	lo0.c${GROUP}.ws.nsrc.org c${GROUP}.ws.nsrc.org c${GROUP}
-100.68.${GROUP}.16	gi0-1.c${GROUP}.ws.nsrc.org
-2001:DB8:${GROUP}:10::	gi0-1.c${GROUP}.ws.nsrc.org
-100.68.${GROUP}.18	gi0-2.c${GROUP}.ws.nsrc.org
-2001:DB8:${GROUP}:11::	gi0-2.c${GROUP}.ws.nsrc.org
-100.68.${GROUP}.20	gi0-3.c${GROUP}.ws.nsrc.org
-2001:DB8:${GROUP}:12::	gi0-3.c${GROUP}.ws.nsrc.org
-100.68.${GROUP}.29	gi0-4.c${GROUP}.ws.nsrc.org
-2001:DB8:${GROUP}:21::29	gi0-4.c${GROUP}.ws.nsrc.org
+100.68.${GROUP}.16	gi1.c${GROUP}.ws.nsrc.org
+2001:DB8:${GROUP}:10::	gi1.c${GROUP}.ws.nsrc.org
+100.68.${GROUP}.18	gi2.c${GROUP}.ws.nsrc.org
+2001:DB8:${GROUP}:11::	gi2.c${GROUP}.ws.nsrc.org
+100.68.${GROUP}.20	gi3.c${GROUP}.ws.nsrc.org
+2001:DB8:${GROUP}:12::	gi3.c${GROUP}.ws.nsrc.org
+100.68.${GROUP}.29	gi4.c${GROUP}.ws.nsrc.org
+2001:DB8:${GROUP}:21::29	gi4.c${GROUP}.ws.nsrc.org
+100.68.${GROUP}.22	gi5.c${GROUP}.ws.nsrc.org
+2001:DB8:${GROUP}:13::	gi5.c${GROUP}.ws.nsrc.org
 
 100.68.${GROUP}.3	lo0.p${GROUP}.ws.nsrc.org p${GROUP}.ws.nsrc.org p${GROUP}
 2001:DB8:${GROUP}::3	lo0.p${GROUP}.ws.nsrc.org p${GROUP}.ws.nsrc.org p${GROUP}
-100.68.${GROUP}.19	gi0-1.p${GROUP}.ws.nsrc.org
-2001:DB8:${GROUP}:11::1	gi0-1.p${GROUP}.ws.nsrc.org
-${PEER_LOCAL_V4}	gi0-2.p${GROUP}.ws.nsrc.org
-${PEER_LOCAL_V6}	gi0-2.p${GROUP}.ws.nsrc.org
-100.127.1.${GROUP}	gi0-3.p${GROUP}.ws.nsrc.org
-2001:DB8:FFFF:1::${GROUP}	gi0-3.p${GROUP}.ws.nsrc.org
-100.127.3.${GROUP}	gi0-4.p${GROUP}.ws.nsrc.org
-2001:DB8:FFFE:1::${GROUP}	gi0-4.p${GROUP}.ws.nsrc.org
+100.68.${GROUP}.19	gi1.p${GROUP}.ws.nsrc.org
+2001:DB8:${GROUP}:11::1	gi1.p${GROUP}.ws.nsrc.org
+${PEER_LOCAL_V4}	gi2.p${GROUP}.ws.nsrc.org
+${PEER_LOCAL_V6}	gi2.p${GROUP}.ws.nsrc.org
+100.127.1.${GROUP}	gi3.p${GROUP}.ws.nsrc.org
+2001:DB8:FFFF:1::${GROUP}	gi3.p${GROUP}.ws.nsrc.org
+100.127.3.${GROUP}	gi4.p${GROUP}.ws.nsrc.org
+2001:DB8:FFFE:1::${GROUP}	gi4.p${GROUP}.ws.nsrc.org
 
 100.68.${GROUP}.4	lo0.a${GROUP}.ws.nsrc.org a${GROUP}.ws.nsrc.org a${GROUP}
 2001:DB8:${GROUP}::4	lo0.a${GROUP}.ws.nsrc.org a${GROUP}.ws.nsrc.org a${GROUP}
-100.68.${GROUP}.21	gi0-1.a${GROUP}.ws.nsrc.org
-2001:DB8:${GROUP}:12::1	gi0-1.a${GROUP}.ws.nsrc.org
-100.68.${GROUP}.34	gi0-2.a${GROUP}.ws.nsrc.org
-2001:DB8:${GROUP}:31::1	gi0-2.a${GROUP}.ws.nsrc.org
-100.68.${GROUP}.36	gi0-3.a${GROUP}.ws.nsrc.org
-2001:DB8:${GROUP}:32::1	gi0-3.a${GROUP}.ws.nsrc.org
+100.68.${GROUP}.21	gi1.a${GROUP}.ws.nsrc.org
+2001:DB8:${GROUP}:12::1	gi1.a${GROUP}.ws.nsrc.org
+100.68.${GROUP}.34	gi2.a${GROUP}.ws.nsrc.org
+2001:DB8:${GROUP}:31::	gi2.a${GROUP}.ws.nsrc.org
 
 100.68.${GROUP}.64	lo0.cust${GROUP}.ws.nsrc.org cust${GROUP}.ws.nsrc.org cust${GROUP}
 2001:DB8:${GROUP}:4000::	lo0.cust${GROUP}.ws.nsrc.org cust${GROUP}.ws.nsrc.org cust${GROUP}
@@ -147,11 +148,15 @@ ${PEER_LOCAL_V6}	gi0-2.p${GROUP}.ws.nsrc.org
 2001:DB8:$(( GROUP+100 ))::1	lo1.cust${GROUP}.ws.nsrc.org
 100.68.${GROUP}.35	gi0-1.cust${GROUP}.ws.nsrc.org
 2001:DB8:${GROUP}:31::1	gi0-1.cust${GROUP}.ws.nsrc.org
-100.68.${OTHER}.37	gi0-2.cust${GROUP}.ws.nsrc.org
-2001:DB8:${OTHER}:32::1	gi0-2.cust${GROUP}.ws.nsrc.org
+
+100.68.${GROUP}.5	lo0.t${GROUP}.ws.nsrc.org t${GROUP}.ws.nsrc.org t${GROUP}
+2001:DB8:${GROUP}::5	lo0.t${GROUP}.ws.nsrc.org t${GROUP}.ws.nsrc.org t${GROUP}
+100.68.${GROUP}.23	gi0-1.t${GROUP}.ws.nsrc.org
+2001:DB8:${GROUP}:13::1	gi0-1.t${GROUP}.ws.nsrc.org
 
 100.68.${GROUP}.30	ens3.srv${GROUP}.ws.nsrc.org
 2001:db8:${GROUP}:21::30	ens3.srv${GROUP}.ws.nsrc.org
 192.168.122.$(( GROUP*10 ))	ens4.srv${GROUP}.ws.nsrc.org srv${GROUP}.ws.nsrc.org srv${GROUP}
 EOS
+done
 done
